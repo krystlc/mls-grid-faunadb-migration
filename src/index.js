365e8e8fa27185bed17e4cffac889ea048635c3a
Alex Valle <alex.valle@gmail.com>
2020-04-04 16:03:20 -0400

    collecting property data from api

--- src/index.js
+++ src/index.js
@@ -2,24 +2,34 @@ import dotenv from 'dotenv'
 import axios from 'axios'
 dotenv.config()
 
-console.log('starting ES6 app...')
+console.log('starting fetch...')
 
 axios.defaults.baseURL = 'https://api.mlsgrid.com'
+axios.defaults.headers.common['Accept-Encoding'] = 'gzip'
 axios.defaults.headers.common['Authorization'] = process.env.ACCESS_TOKEN
 
-async function getProperties() {
+let pages = 0
+const arr = []
+async function getProperties(url) {
   try {
-    const response = await axios.get('/PropertyResi', {
-      params: {
-        $filter: 'ModificationTimestamp gt 2020-04-03T23:59:59.99Z'
+    pages++
+    const response = await axios.get(url)
+    const { value } = response.data
+    if (value) {
+      arr.push(...value)
+      console.log(`PAGE no. ${pages}: `, value.length)
+      const nextLink = response.data['@odata.nextLink']
+      if (nextLink) {
+        setTimeout(() => getProperties(nextLink), 2000)
       }
-    })
-    console.log('yes!', response)
+    }
   } catch (error) {
-    console.log('nooooo', error.message)
+    console.log('ERR:', error.reponse.message)
   } finally {
-    console.log('donzo!')
+    console.log('DONZO!')
+    console.log(arr)
   }
 }
 
-getProperties()
+const startLink = `${axios.defaults.baseURL}/PropertyResi?$filter=${encodeURI('MlgCanView eq true and ModificationTimestamp gt 2020-04-03T23:59:59.99Z')}&$skip=13000`
+getProperties(startLink)
