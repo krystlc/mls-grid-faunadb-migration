56f34430d3f1737e84d4e51e9514e8c448825d94
Alex Valle <alex.valle@gmail.com>
2020-04-04 12:14:58 -0400

    created axios request

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
 import dotenv from 'dotenv'
+import axios from 'axios'
 dotenv.config()
 
 console.log('starting ES6 app...')
-console.log('base url:', process.env.BASE_URL)
+
+axios.defaults.baseURL = 'https://api.mlsgrid.com'
+axios.defaults.headers.common['Authorization'] = process.env.ACCESS_TOKEN
+
+async function getProperties() {
+  try {
+    const response = await axios.get('/PropertyResi', {
+      params: {
+        $filter: 'ModificationTimestamp gt 2020-04-03T23:59:59.99Z'
+      }
+    })
+    console.log('yes!', response)
+  } catch (error) {
+    console.log('nooooo', error.message)
+  } finally {
+    console.log('donzo!')
+  }
+}
+
+getProperties()
