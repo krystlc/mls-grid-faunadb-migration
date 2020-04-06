a5511dfd23c6aace63abbcb1c575eb88f3894951
Alex Valle <alex.valle@gmail.com>
2020-04-06 17:04:11 -0400

    added build and dev scripts; ready to create property documents

--- src/index.js
+++ src/index.js
@@ -1,35 +1,92 @@
 import dotenv from 'dotenv'
 import axios from 'axios'
+
 dotenv.config()
 
-console.log('starting fetch...')
+const timestamp = new Date()
+timestamp.setDate(timestamp.getDate() - 1)
 
-axios.defaults.baseURL = 'https://api.mlsgrid.com'
-axios.defaults.headers.common['Accept-Encoding'] = 'gzip'
-axios.defaults.headers.common['Authorization'] = process.env.ACCESS_TOKEN
+console.log('starting app...', timestamp)
 
-let pages = 0
-const arr = []
-async function getProperties(url) {
+const instanceMlsGrid = axios.create({
+  baseURL: 'https://api.mlsgrid.com',
+  headers: {
+    'Authorization': `Bearer ${process.env.MLS_GRID_ACCESS_TOKEN}`,
+    'Accept-Enconding': 'gzip'
+  }
+})
+
+const instanceFaunaDb = axios.create({
+  baseURL: 'https://graphql.fauna.com/graphql',
+  headers: {
+    'Authorization': `Bearer ${process.env.FAUNA_DB_ACCESS_TOKEN}`
+  }
+})
+
+const counties = ['Hillsborough', 'Pasco'] // , 'Pinellas'
+
+const getProperties = async (url) => {
+  console.log('\nattempting to fetch properties...\n')
   try {
-    pages++
-    const response = await axios.get(url)
-    const { value } = response.data
-    if (value) {
-      arr.push(...value)
-      console.log(`PAGE no. ${pages}: `, value.length)
-      const nextLink = response.data['@odata.nextLink']
-      if (nextLink) {
-        setTimeout(() => getProperties(nextLink), 2000)
-      }
-    }
+    // const response = await instanceMlsGrid.get(url)
+    const response = require(`../data/${url}`)
+    return response
   } catch (error) {
-    console.log('ERR:', error.reponse.message)
-  } finally {
-    console.log('DONZO!')
-    console.log(arr)
+    throw error
   }
 }
 
-const startLink = `${axios.defaults.baseURL}/PropertyResi?$filter=${encodeURI('MlgCanView eq true and ModificationTimestamp gt 2020-04-03T23:59:59.99Z')}&$skip=13000`
-getProperties(startLink)
+const getAllProperties = async (url) => {
+  const response = await getProperties(url)
+  let data = [...new Set(response.data.value)]
+  const nextLink = response.data['@odata.nextLink']
+  if (nextLink) {
+    const nextResponse = await getAllProperties(nextLink)
+    data = [...data, ...nextResponse]
+  }
+  return data
+}
+
+const createProperty = async (property) => {
+  console.log('creating property in fauna db...')
+  const data = Object.keys(property).map(key => {
+    return `\n${key}: ${JSON.stringify(property[key])}`
+  }).join()
+  const query = `mutation { 
+      createProperty(data: {
+        ${data}
+      }
+    ){
+      _id
+    }
+  }`
+  try {
+    const response = await instanceFaunaDb({
+      method: 'post',
+      data: JSON.stringify({ query: `${query}` })
+    })
+    return response.data
+  } catch (error) {
+    throw error
+  }
+}
+
+const createProperties = async() => {
+  // const startLink = `${instanceMlsGrid.defaults.baseURL}/PropertyResi?$filter=${encodeURI('MlgCanView eq true and ModificationTimestamp gt ${timestamp}')}&$skip=10000`
+  const startLink = 'test.json'
+  const allProperties = await getAllProperties(startLink)
+  return allProperties
+    .filter(({ CountyOrParish }) => counties.includes(CountyOrParish))
+    .map(property => {
+      property.propertyId = property['@odata.id']
+      delete property['@odata.id']
+      return property
+    })
+}
+
+createProperties().then(list => {
+  // console.log('list', list.length)
+  createProperty(list[0]).then(response => {
+    console.log('created property', response)
+  })
+})
