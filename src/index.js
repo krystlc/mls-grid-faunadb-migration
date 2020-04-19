d1d74254fc872b33bf4504f853dc979d44f5adc5
Alex Valle <alex.valle@gmail.com>
2020-04-19 16:05:10 -0400

    increased node memory; seems to work ok

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ console.time('app')
 async function updateDb() {
   console.timeLog('app')
   await mlsgrid.upsertProperties()
+  console.timeLog('app')
+  await mlsgrid.deleteProperties()
 }
 
 updateDb().then(() => {
